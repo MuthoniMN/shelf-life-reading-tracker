c2ca95f449b84d1fb0f926c332abbd0523a1d73a
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-21 20:11:30 +0300

    Complete hero section for the home page

---
+++ client/src/components/HomeHero/HomeHero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from "../Button";
+
+const HomeHero = () => {
+    return (
+        <section className="flex home-hero">
+            <div>
+                <h1>Your Digital Reading Partner</h1>
+                <p>Keep track of the books you read weekly, monthly and yearly. Organize your reading list based on your favorite genres</p>
+                <Button text={"Get Started"} type="cta" />
+            </div>
+            <img width={500} src="https://images.unsplash.com/photo-1612477954469-c6a60de89802?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHJlYWRpbmd8ZW58MHx8MHx8fDA%3D" alt="Reading a book" />
+        </section>
+    )
+}
+
+export default HomeHero
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
-import Button from "../components/Button";
+import HomeHero from "../components/HomeHero/HomeHero";
 
 export default function Home() {
-    return(
-        <div className="home-hero">
-            <h1>Your Digital Reading Partner</h1>
-            <p>Keep track of the books you read weekly, monthly and yearly. Organize your reading list based on your favorite genres</p>
-            <Button text={"Get Started"} type="cta" />
-        </div>
+    return (
+        <HomeHero />
     )
 }
