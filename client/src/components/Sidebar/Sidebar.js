07521ad3b41ff6de8e01865972b490c5e15f0f5e
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-22 21:44:36 +0300

    sidebar component

---
+++ client/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Sidebar.css"
+
+const Sidebar = ({links, header, current, setCurrent}) => {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li>
+                    <h3>{header}</h3>
+                </li>
+                {links.map(a => (
+                    <li>
+                        <p className={current === a ? "active" : "genres"} onClick={() => setCurrent(a)}>{a}</p>
+                    </li>
+                ))}
+                <li>
+                    <h3>Recommendations</h3>
+                </li>
+                <li>
+                    <p>Best Sellers</p>
+                    <p>Writer of the Month</p>
+                    <p>Trending</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
