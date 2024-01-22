d6799de4c69d29f2c4ed3914ef68547810ebe1b4
Michelle Ndiangui <ndianguimichelle@gmail.com>
2024-01-22 21:44:19 +0300

    adding explore section

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import HomeHero from "../components/HomeHero/HomeHero";
+import Explore from "../components/Explore/Explore";
 
 export default function Home() {
     return (
-        <HomeHero />
+        <main>
+            <HomeHero />
+            <Explore />
+        </main>
     )
 }
